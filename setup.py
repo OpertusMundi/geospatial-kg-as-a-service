519ecf8b57aaafa7edf6deebae26e1d85844afec
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2021-01-18 11:52:28 +0100

    Add basic project structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='geospatial-kg-as-a-service',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Patrick Westphal',
+    author_email='',
+    description='',
+    install_requires=[
+        'fastapi==0.55.1',
+        'uvicorn==0.12.3',
+    ]
+)
