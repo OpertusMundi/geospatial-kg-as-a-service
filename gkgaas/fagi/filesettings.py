ed8b4438ba2a37ba15370285fbd56f8a332fdd3d
Patrick <patrick.westphal@informatik.uni-leipzig.de>
2022-02-04 11:01:26 +0100

    Add FAGI (in-/output/links) file settings + tests

---
+++ gkgaas/fagi/filesettings.py
@@ -0,0 +1,108 @@
+from datetime import date
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+
+from starlette.datastructures import URL
+
+from gkgaas.fagi import LinksFormat, FusionMode
+
+
+@dataclass
+class FileSettings(ABC):
+    id: str
+    _prefix: str = field(init=False)
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+@dataclass
+class InputDatasetSettings(FileSettings):
+    # categories: List[str] = None  FIXME: not clear how to use this
+    date: date = None
+    file_path: str = None
+    endpoint: URL = None
+
+    def __str__(self):
+        # self.id
+        settings_str = f'        <id>{self.id}</id>'
+
+        # self.date
+        if self.date is not None:
+            date_str = self.date.strftime('%Y-%m-%d')
+            settings_str += f'\n        <date>{date_str}</date>'
+
+        # self.file_path
+        if self.file_path is not None:
+            settings_str += f'\n        <file>{self.file_path}</file>'
+
+        # self.endpoint
+        else:
+            if self.endpoint is None:
+                raise Exception(
+                    'Either file path or SPARQL endpoint has to be configured')
+
+            settings_str += f'\n        <endpoint>{str(self.endpoint)}</endpoint>'
+
+        return f'    <{self._prefix}>\n' + \
+               settings_str + \
+               f'\n    </{self._prefix}>'
+
+
+class LeftInputDatasetSettings(InputDatasetSettings):
+    _prefix = 'left'
+
+
+class RightInputDatasetSettings(InputDatasetSettings):
+    _prefix = 'right'
+
+
+@dataclass
+class LinksFileSettings(FileSettings):
+    file_path: str
+    links_format: LinksFormat
+
+    _prefix = 'links'
+
+    def __str__(self):
+        return f"""    <{self._prefix}>
+        <id>{self.id}</id>
+        <file>{self.file_path}</file>
+        <linksFormat>{str(self.links_format)}</linksFormat>
+    </{self._prefix}>"""
+
+
+@dataclass
+class FusedOutputDatasetSettings(FileSettings):
+    mode: FusionMode
+    output_dir: str
+
+    # Optional. Specifies the output file path of the fused dataset (based on
+    # the fusion mode).
+    fused: str = 'fused.nt'
+
+    # Optional. Specifies the output file path of the non-fused dataset (based
+    # on the fusion mode).
+    remaining: str = 'remaining.nt'
+
+    # Optional. Specifies the output file path of the dataset containing
+    # ambiguous linked entities.
+    ambiguous: str = 'ambiguous.nt'
+
+    statistics: str = 'statistics.txt'
+    fusion_log: str = 'fusionLog.txt'
+
+    _prefix = 'target'
+
+    def __str__(self):
+        return f"""    <{self._prefix}>
+        <id>{self.id}</id>
+        <mode>{str(self.mode)}</mode>
+        <outputDir>{self.output_dir}</outputDir>
+        <fused>{self.fused}</fused>
+        <remaining>{self.remaining}</remaining>
+        <ambiguous>{self.ambiguous}</ambiguous>
+        <statistics>{self.statistics}</statistics>
+        <fusionLog>{self.fusion_log}</fusionLog>
+    </{self._prefix}>"""
---
+++ tests/fagi/filesettingstests.py
@@ -0,0 +1,242 @@
+import unittest
+from datetime import date
+
+from starlette.datastructures import URL
+
+from gkgaas.fagi import LinksFormat, FusionMode
+from gkgaas.fagi.filesettings import LinksFileSettings, \
+    LeftInputDatasetSettings, RightInputDatasetSettings, \
+    FusedOutputDatasetSettings
+
+
+class LeftInputDatasetSettingsTests(unittest.TestCase):
+    def test_to_str_01(self):
+        expected_config_str = """    <left>
+        <id>a</id>
+        <date>2022-06-01</date>
+        <file>/path/to/dataset.nt</file>
+    </left>"""
+
+        input_dataset_settings = LeftInputDatasetSettings(
+            id='a',
+            date=date(year=2022, month=6, day=1),
+            file_path='/path/to/dataset.nt'
+        )
+
+        self.assertEqual(expected_config_str, str(input_dataset_settings))
+
+    def test_to_str_02(self):
+        expected_config_str = """    <left>
+        <id>b</id>
+        <date>2022-05-13</date>
+        <endpoint>http://dbpedia.org/sparql</endpoint>
+    </left>"""
+
+        input_dataset_settings = LeftInputDatasetSettings(
+            id='b',
+            date=date(year=2022, month=5, day=13),
+            endpoint=URL('http://dbpedia.org/sparql')
+        )
+
+        self.assertEqual(expected_config_str, str(input_dataset_settings))
+
+    def test_to_str_03(self):
+        expected_config_str = """    <left>
+        <id>a</id>
+        <file>/path/to/dataset.nt</file>
+    </left>"""
+
+        input_dataset_settings = LeftInputDatasetSettings(
+            id='a',
+            file_path='/path/to/dataset.nt'
+        )
+
+        self.assertEqual(expected_config_str, str(input_dataset_settings))
+
+    def test_to_str_04(self):
+        input_dataset_settings = LeftInputDatasetSettings(
+            id='a'
+        )
+
+        self.assertRaises(Exception, input_dataset_settings.__str__)
+
+
+class RightInputDatasetSettingsTests(unittest.TestCase):
+    def test_to_str_01(self):
+        expected_config_str = """    <right>
+        <id>a</id>
+        <date>2022-06-01</date>
+        <file>/path/to/dataset.nt</file>
+    </right>"""
+
+        input_dataset_settings = RightInputDatasetSettings(
+            id='a',
+            date=date(year=2022, month=6, day=1),
+            file_path='/path/to/dataset.nt'
+        )
+
+        self.assertEqual(expected_config_str, str(input_dataset_settings))
+
+    def test_to_str_02(self):
+        expected_config_str = """    <right>
+        <id>b</id>
+        <date>2022-05-13</date>
+        <endpoint>http://dbpedia.org/sparql</endpoint>
+    </right>"""
+
+        input_dataset_settings = RightInputDatasetSettings(
+            id='b',
+            date=date(year=2022, month=5, day=13),
+            endpoint=URL('http://dbpedia.org/sparql')
+        )
+
+        self.assertEqual(expected_config_str, str(input_dataset_settings))
+
+    def test_to_str_03(self):
+        expected_config_str = """    <right>
+        <id>a</id>
+        <file>/path/to/dataset.nt</file>
+    </right>"""
+
+        input_dataset_settings = RightInputDatasetSettings(
+            id='a',
+            file_path='/path/to/dataset.nt'
+        )
+
+        self.assertEqual(expected_config_str, str(input_dataset_settings))
+
+    def test_to_str_04(self):
+        input_dataset_settings = RightInputDatasetSettings(
+            id='a'
+        )
+
+        self.assertRaises(Exception, input_dataset_settings.__str__)
+
+
+class TestLinksFileSettings(unittest.TestCase):
+    def test_to_str_01(self):
+        expected_config_str = """    <links>
+        <id>links</id>
+        <file>/path/to/file.nt</file>
+        <linksFormat>nt</linksFormat>
+    </links>"""
+
+        links_file_settings = LinksFileSettings(
+            id='links',
+            file_path='/path/to/file.nt',
+            links_format=LinksFormat.NT
+        )
+
+        self.assertEqual(expected_config_str, str(links_file_settings))
+
+    def test_to_str_02(self):
+        expected_config_str = """    <links>
+        <id>links</id>
+        <file>/path/to/file.csv</file>
+        <linksFormat>csv</linksFormat>
+    </links>"""
+
+        links_file_settings = LinksFileSettings(
+            id='links',
+            file_path='/path/to/file.csv',
+            links_format=LinksFormat.CSV
+        )
+
+        self.assertEqual(expected_config_str, str(links_file_settings))
+
+    def test_to_str_03(self):
+        expected_config_str = """    <links>
+        <id>links</id>
+        <file>/path/to/file.csv</file>
+        <linksFormat>csv-unique-links</linksFormat>
+    </links>"""
+
+        links_file_settings = LinksFileSettings(
+            id='links',
+            file_path='/path/to/file.csv',
+            links_format=LinksFormat.CSV_UNIQUE_LINKS
+        )
+
+        self.assertEqual(expected_config_str, str(links_file_settings))
+
+    def test_to_str_04(self):
+        expected_config_str = """    <links>
+        <id>links</id>
+        <file>/path/to/file.csv</file>
+        <linksFormat>csv-ensembles</linksFormat>
+    </links>"""
+
+        links_file_settings = LinksFileSettings(
+            id='links',
+            file_path='/path/to/file.csv',
+            links_format=LinksFormat.CSV_ENSEMBLES
+        )
+
+        self.assertEqual(expected_config_str, str(links_file_settings))
+
+
+class TestFusedOutputDatasetSettings(unittest.TestCase):
+    def test_to_str_01(self):
+        expected_settings_str = """    <target>
+        <id>fused</id>
+        <mode>aa_mode</mode>
+        <outputDir>/path/to/dir/</outputDir>
+        <fused>fused.nt</fused>
+        <remaining>remaining.nt</remaining>
+        <ambiguous>ambiguous.nt</ambiguous>
+        <statistics>statistics.txt</statistics>
+        <fusionLog>fusionLog.txt</fusionLog>
+    </target>"""
+
+        output_dataset_settings = FusedOutputDatasetSettings(
+            id='fused',
+            mode=FusionMode.AA,
+            output_dir='/path/to/dir/'
+        )
+
+        self.assertEqual(expected_settings_str, str(output_dataset_settings))
+
+    def test_to_str_02(self):
+        expected_settings_str = """    <target>
+        <id>fused</id>
+        <mode>bb_mode</mode>
+        <outputDir>/path/to/dir/</outputDir>
+        <fused>fused_file.nt</fused>
+        <remaining>remaining_file.nt</remaining>
+        <ambiguous>ambiguous_file.nt</ambiguous>
+        <statistics>statistics_file.txt</statistics>
+        <fusionLog>fusion_log.txt</fusionLog>
+    </target>"""
+
+        output_dataset_settings = FusedOutputDatasetSettings(
+            id='fused',
+            mode=FusionMode.BB,
+            output_dir='/path/to/dir/',
+            fused='fused_file.nt',
+            remaining='remaining_file.nt',
+            ambiguous='ambiguous_file.nt',
+            statistics='statistics_file.txt',
+            fusion_log='fusion_log.txt'
+        )
+
+        self.assertEqual(expected_settings_str, str(output_dataset_settings))
+
+    def test_to_str_03(self):
+        expected_settings_str = """    <target>
+        <id>fused</id>
+        <mode>l_mode</mode>
+        <outputDir>/path/to/dir/</outputDir>
+        <fused>fused.nt</fused>
+        <remaining>remaining.nt</remaining>
+        <ambiguous>ambiguous.nt</ambiguous>
+        <statistics>statistics.txt</statistics>
+        <fusionLog>fusionLog.txt</fusionLog>
+    </target>"""
+
+        output_dataset_settings = FusedOutputDatasetSettings(
+            id='fused',
+            mode=FusionMode.L,
+            output_dir='/path/to/dir/'
+        )
+
+        self.assertEqual(expected_settings_str, str(output_dataset_settings))
