4d5134fe4eb8976366ddf6f8e0554422c6a290b8
Patrick <patrick.westphal@informatik.uni-leipzig.de>
2022-03-15 11:23:23 +0100

    Add CSV serialization method to classification

--- gkgaas/triplegeo/classification.py
+++ gkgaas/triplegeo/classification.py
@@ -19,14 +19,33 @@ class Category(object):
 
         return res
 
+    def _csv_str__(self):
+        nl = os.linesep
+        return nl.join(
+            map(
+                lambda c:
+                f'"{self.category_id}","{self.name}","{c[1]}","{c[0]}"',
+                self.sub_classes))
+
 
 @dataclass
 class ClassificationSpecification(object):
     file_name: str
     categories: List[Category]
 
+    def to_csv_file(self, target_dir_path):
+        file_path = os.path.join(target_dir_path, self.file_name)
+        nl = os.linesep
+
+        with open(file_path, 'w') as out_file:
+            out_file.write(
+                '"type","maincategory_en","combotype","subcategory_en"' + nl)
+
+            out_file.writelines(
+                [f'{c._csv_str__()}{nl}' for c in self.categories])
+
     def to_yml_file(self, target_dir_path):
-        file_path = target_dir_path + os.sep + self.file_name
+        file_path = os.path.join(target_dir_path, self.file_name)
         nl = os.linesep
 
         with open(file_path, 'w') as out_file:
