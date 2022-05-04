84ddc5f79c67822f49466c1373180f03fc027b9d
Patrick <patrick.westphal@informatik.uni-leipzig.de>
2022-05-05 00:53:20 +0200

    Add PyDantic models
    One for the information posted to the KG conversion, linking and fusion REST endpoint, and one capturing the return information after the conversion, linking and fusion

--- gkgaas/model/__init__.py
+++ gkgaas/model/__init__.py
@@ -8,3 +8,14 @@ class ConversionDescription(BaseModel):
     conversion_profile_name: str
     file_to_link_with: Optional[str]
     linking_profile_name: Optional[str]
+
+
+class KnowledgeGraphConversionInformation(BaseModel):
+    input_file_address: str
+    conversion_profile_name: str
+    topio_kg_address: str
+
+
+class KnowledgeGraphInfo(BaseModel):
+    kg_address: str
+    topio_kg_address: str
