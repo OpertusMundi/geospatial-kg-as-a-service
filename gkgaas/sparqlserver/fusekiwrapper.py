e8f8fe226105d4778bd9bc9cff992d5a99030f01
Patrick <patrick.westphal@informatik.uni-leipzig.de>
2022-06-23 14:22:55 +0200

    Extend functionality of FusekiWrapper

--- gkgaas/sparqlserver/fusekiwrapper.py
+++ gkgaas/sparqlserver/fusekiwrapper.py
@@ -1,9 +1,15 @@
+import logging
 import os
 import subprocess
 import time
 from typing import List
 
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+from gkgaas.exceptions import WrongExecutablePath
 from gkgaas.sparqlserver import SPARQLServer
+
+logger = logging.getLogger(__name__)
 
 
 class FusekiWrapper(SPARQLServer):
@@ -13,12 +19,27 @@ class FusekiWrapper(SPARQLServer):
             rdf_file_paths: List[str],
             port: int = 8080):
 
+        if not os.path.exists(path_to_fuseki_executable):
+            raise WrongExecutablePath(
+                f'Fuseki executable path {path_to_fuseki_executable} does '
+                f'not exist'
+            )
+
+        for rdf_file_path in rdf_file_paths:
+            if not os.path.exists(rdf_file_path):
+                raise FileNotFoundError()
+
         self.path_to_fuseki_executable = path_to_fuseki_executable
         self.rdf_file_paths = rdf_file_paths
         self.port = port
         self.server_process = None
 
-    def run(self) -> str:
+    def _get_query_url(self):
+        hostname = os.uname().nodename
+
+        return f'http://{hostname}:{self.port}/kg/sparql'
+
+    def run(self):
         if self.server_process is not None:
             raise Exception('Service already running')
 
@@ -39,10 +60,17 @@ class FusekiWrapper(SPARQLServer):
         fuseki_dir = os.path.dirname(self.path_to_fuseki_executable)
         self.server_process = subprocess.Popen(args, cwd=fuseki_dir)
 
-        hostname = os.uname().nodename
-
-        return f'http://{hostname}:{self.port}/'
+        logger.info(f'SPARQL server started at {self._get_query_url()}')
 
     def stop(self):
+        logger.info('Stopping SPARQL server')
         if self.server_process is not None:
             self.server_process.kill()
+
+    def query(self, sparql_query: str):
+        url = self._get_query_url()
+        sparql_endpoint = SPARQLWrapper(url)
+        sparql_endpoint.setReturnFormat(JSON)
+        sparql_endpoint.setQuery(sparql_query)
+
+        return sparql_endpoint.queryAndConvert()
