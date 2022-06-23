4c3affbbf179d48226459db9431d66d717367040
Patrick <patrick.westphal@informatik.uni-leipzig.de>
2022-06-23 14:15:17 +0200

    Add custom exception

--- gkgaas/exceptions/__init__.py
+++ gkgaas/exceptions/__init__.py
@@ -4,3 +4,15 @@ class WrongExecutablePath(Exception):
 
 class RunnerExecutionFailed(Exception):
     pass
+
+
+class ProtocolNotSupportedException(Exception):
+    """
+    This is exception is thrown when a Tpio ID is translated to a local ID
+    which cannot be handled by the Topio KG service. This could be, e.g. when
+    the local ID is an HDFS identifire like hdfs:///org/my_file.nt which cannot
+    be resolved without HDFS support
+    """
+
+    def __init__(self, msg):
+        self.msg = msg
